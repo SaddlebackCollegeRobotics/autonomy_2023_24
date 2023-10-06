fc7eb8761afefb0479424aa2cc4709d24a19ddda
Supernova1114 <supernovawarriors128@gmail.com>
2023-10-05 20:44:02 -0700

    Removed main method

--- gps/src/gps_driver/gps_driver/ublox_gps_driver.py
+++ gps/src/gps_driver/gps_driver/ublox_gps_driver.py
@@ -5,7 +5,11 @@ import serial
 class GPSModel:
 
     def __init__(self, dev_path: str):
-        self.gps = UbloxGps(serial.Serial("dev_path", baudrate=38400, timeout=1))
+        try:
+            self.gps = UbloxGps(serial.Serial(dev_path, baudrate=38400, timeout=1))
+        except Exception:
+            print("Error: Could not open serial port.")
+            exit(1)
 
     def get_coords(self):
         try:
@@ -14,17 +18,3 @@ class GPSModel:
         except (ValueError, IOError):
             print("Error: Could not parse GPS coords.")
         return None
-
-
-def main():
-    gps = GPSModel('/dev/ttyACM0')
-
-    while True:
-        try:
-            lat, lon = gps.get_coords()
-            print("Rel Latitude: {:.7f}, Rel Longitude: {:.7f}".format(lat, lon))
-        except KeyboardInterrupt:
-            print("exit")
-
-if __name__ == '__main__':
-    main()
