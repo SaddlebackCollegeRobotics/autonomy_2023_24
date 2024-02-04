02dbd485cd9cb3628d797fad6e17c0f685f788ed
Supernova1114 <supernovawarriors128@gmail.com>
2024-02-04 15:40:50 -0800

    twist visualization testing

--- nav2/sam_bot_description/launch/display.launch.py
+++ nav2/sam_bot_description/launch/display.launch.py
@@ -69,7 +69,7 @@ def generate_launch_description():
         # joint_state_publisher_gui_node,
         robot_state_publisher_node,
         ekf_filter_local_node,
-        ekf_filter_global_node,
+        # ekf_filter_global_node,
         navsat_transform_node,
         rviz_node
     ])
