1a866123419a8841c796c0e8644f5c3468ba0dbb
Supernova1114 <supernovawarriors128@gmail.com>
2024-02-29 13:08:41 -0800

    Added delay to spiral search

--- spiral-search/spiral/spiral_driver.py
+++ spiral-search/spiral/spiral_driver.py
@@ -1,5 +1,6 @@
 import rclpy
 from rclpy.node import Node
+from std_msgs.msg import Float64MultiArray
 from geometry_msgs.msg import Twist, Vector3
 
 from time import sleep
@@ -22,14 +23,20 @@ class SpiralDriver(Node):
 
         print("Spiral driver node started.")
 
-        self._publisher = self.create_publisher(Twist, '/cmd_vel', 10)
+        self._publisher = self.create_publisher(Float64MultiArray, '/drive/control_input', 10)
 
-        self._twist_msg = Twist()
+        self._vel_msg = Float64MultiArray()
 
-        loop_rate = self.create_rate(SpiralDriver.LOOP_RATE, self.get_clock())
+        # loop_rate = self.create_rate(SpiralDriver.LOOP_RATE, self.get_clock())
+
+        # Temp delay for operator to clear area
+        for _ in range(20):
+            self._vel_msg.data = [0.0, 0.0]
+            self._publisher.publish(self._vel_msg)
+            sleep(0.5)
 
         # Send messages to rotate for a full second
-        TURN_IN_PLACE_TIME: int = 10
+        TURN_IN_PLACE_TIME: int = 7
         from math import pi
         for _ in range(TURN_IN_PLACE_TIME * SpiralDriver.LOOP_RATE):
             self.send_rotate_msg(2 * pi / TURN_IN_PLACE_TIME)
@@ -37,14 +44,13 @@ class SpiralDriver(Node):
 
         # Keep spiraling forever, keep track of current iteration to make spiral larger
         #TODO: Change this sometime
-        INITIAL_ROT_SPEED: float = 3.0
-        SPIRAL_RATE: float = 499/500 # Constant to multiply rotation speed by on each iteration
-        current_rot_speed: float = INITIAL_ROT_SPEED
+        CURVE_RATE: float = 2499/2500
+        current_curve_amt: float = 99/100
         while True:
-            self.send_spiral_msg(current_rot_speed)
+            self.send_spiral_msg(current_curve_amt)
             # loop_rate.sleep()
             sleep(1/SpiralDriver.LOOP_RATE)
-            current_rot_speed *= SPIRAL_RATE
+            current_curve_amt *= CURVE_RATE
 
     def send_rotate_msg(self, speed: float):
         """This method sends a Twist message to rotate the rover in-place,
@@ -54,12 +60,11 @@ class SpiralDriver(Node):
             speed (float): Angular speed to rotate at, in rad/s.
         """
         print(f"Sending rotate message with speed {speed}")
-        self._twist_msg.linear = Vector3(x=0.0, y=0.0, z=0.0)
-        self._twist_msg.angular = Vector3(x=0.0, y=0.0, z=speed)
+        self._vel_msg.data = [-1.0, 1.0]
 
-        self._publisher.publish(self._twist_msg)
+        self._publisher.publish(self._vel_msg)
     
-    def send_spiral_msg(self, rotation_speed: float, linear_speed: float = 20.0):
+    def send_spiral_msg(self, curve_amt: float):
         """This method sends a Twist message to drive the rover at a unit vector forward and
         the specified `rotation_speed`. Rotation speed should be decreased over time as to
         create a spiral pattern.
@@ -67,11 +72,10 @@ class SpiralDriver(Node):
         Args:
             rotation_speed (float): How fast the rover should rotate in rad/s.
         """
-        print(f"Sending spiral message with linear speed {linear_speed} and rotation speed {rotation_speed}")
-        self._twist_msg.linear = Vector3(x=linear_speed, y=0.0, z=0.0)
-        self._twist_msg.angular = Vector3(x=0.0, y=0.0, z=rotation_speed)
+        # print(f"Sending spiral message with linear speed {linear_speed} and rotation speed {rotation_speed}")
+        self._vel_msg.data = [1.0 - 1.0 * curve_amt, 1.0 ]
 
-        self._publisher.publish(self._twist_msg)
+        self._publisher.publish(self._vel_msg)
 
 
 
