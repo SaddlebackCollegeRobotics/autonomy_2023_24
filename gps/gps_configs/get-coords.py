b5469584b6b8dafd5ee264c7a1fbad67f94054e7
Supernova1114 <supernovawarriors128@gmail.com>
2024-03-06 16:55:53 -0800

    Added gps configuration files

---
+++ gps/gps_configs/get-coords.py
@@ -0,0 +1,10 @@
+from ublox_gps_driver import GPS_Model
+from time import sleep
+
+gps = GPS_Model("/dev/ttyACM0")
+
+while True:
+    coords = gps.get_coords()
+    if coords is not None:
+        print(f'{coords[0]:.7f}, {coords[1]:.7f}')
+    sleep(1)
