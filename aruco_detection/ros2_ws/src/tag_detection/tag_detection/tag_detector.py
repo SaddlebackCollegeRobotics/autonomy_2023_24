159efd5a95a69ac015b5f9df4f79beb3cb31860a
Saddleback Robotics <saddlebackroverteam@gmail.com>
2024-05-24 00:32:32 -0700

    wip publishing DetectedObjects

--- aruco_detection/ros2_ws/src/tag_detection/tag_detection/tag_detector.py
+++ aruco_detection/ros2_ws/src/tag_detection/tag_detection/tag_detector.py
@@ -2,10 +2,10 @@ import rclpy
 from rclpy.node import Node
 from rclpy.qos import qos_profile_sensor_data
 
+from interfaces.msg import DetectedObject
+
 from sensor_msgs.msg import Image
 from sensor_msgs.msg import CompressedImage
-from tag_interface.msg import Tag as TagData
-from tag_interface.msg import TagList 
 
 from cv_bridge import CvBridge
 import numpy as np
@@ -24,8 +24,8 @@ class Tag_Detector(Node):
         self.subscription = self.create_subscription(Image, 'autonomy/camera_image', self.listener_callback, 10)
         self.bridge = CvBridge()
 
-        self.publisher_ = self.create_publisher(TagList, 'autonomy/tag_data', 10)
-        self.cam_publisher_ = self.create_publisher(CompressedImage, 'autonomy/tag_visualization', qos_profile=qos_profile_sensor_data)
+        self.detected_object_publisher_ = self.create_publisher(DetectedObject, '/object_detection/output', qos_profile=qos_profile_sensor_data)
+        # self.cam_publisher_ = self.create_publisher(CompressedImage, 'autonomy/tag_visualization', qos_profile=qos_profile_sensor_data)
         
         # timer_period = 0.01  # seconds
         timer_period = 0.1
@@ -33,38 +33,37 @@ class Tag_Detector(Node):
         self.detector = Aruco_Detector(DATA_NAME, PATH_TO_DATA, is_stereo=False)       
 
         self.frame = np.zeros((2, 2))
+        self.msg = DetectedObject()
 
     def listener_callback(self, image_msg):
         self.frame = self.bridge.imgmsg_to_cv2(image_msg)
 
-
     def timer_callback(self):
         if self.frame.shape == (2, 2): return
 
-        tag_list = TagList()
-        tag_list.data = []
-        
         tags, frame = self.detector.detect_tags(self.frame) 
-
+        
         for tag in tags:
-            tag_msg = TagData()
-            tag_msg.id = int(tag.id)
-            tag_msg.x_position = tag.x_pos
-            tag_msg.y_position = tag.y_pos
-            tag_msg.distance = tag.dist
-            tag_list.data.append(tag_msg)
+            self.msg.object_name = "aruco_tag" + tag.id
+            self.msg.bottom_left.x, self.msg.top_right.x = tag.x_pos
+            self.msg.bottom_left.y, self.msg.top_right.y = tag.y_pos
+            self.msg.dist = tag.dist
+
+        # TODO : make compatible with message type
+        for tag in tags:
+            self.detected_object_publisher_.publish(self.msg)
 
         #self.get_logger().info(f"tags found: {len(tag_list.data)}")
-        self.publisher_.publish(tag_list)
+        # self.publisher_.publish(tag_list)
 
-        if len(tags) > 0:
-            self.get_logger().info(f"dist: {tags[0].dist}")
-        else:   
-            self.get_logger().info(f"searching for tags")
+        # if len(tags) > 0:
+        #     self.get_logger().info(f"dist: {tags[0].dist}")
+        # else:   
+        #     self.get_logger().info(f"searching for tags")
 
 
-        frame = cv2.resize(frame, (640, 360), interpolation=cv2.INTER_AREA)
-        self.cam_publisher_.publish(self.bridge.cv2_to_compressed_imgmsg(frame))
+        # frame = cv2.resize(frame, (640, 360), interpolation=cv2.INTER_AREA)
+        # self.cam_publisher_.publish(self.bridge.cv2_to_compressed_imgmsg(frame))
 
 
 
